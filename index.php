0e4bd1f04a22615475972930e98139980fe8f8b3
hososo <hoso6616@gmail.com>
2022-04-12 23:27:37 +0900

    4/12HTML基本部分

--- index.php
+++ index.php
@@ -4,9 +4,43 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>会員登録</title>
 </head>
 <body>
-  
+  <header>
+    <h1>会員登録</h1>
+  </header>
+  <main>
+  <p>次のフォームに必要事項をご記入ください</p>
+  <ol>
+    <li>
+      <p>ニックネーム</p>
+      <form>
+        <input type="text">
+      </form>
+    </li>
+    <li>
+      <p>メールアドレス</p>
+      <form>
+        <input type=" mail">
+      </form>
+    </li>
+    <li>
+      <p>パスワード</p>
+      <form>
+        <input type="text">
+      </form>
+    </li>
+    <li>
+      <p>写真など</p>
+      <form>
+        <input type="file">
+      </form>
+    </li>
+  </ol>
+  </main>
+  <footer>
+
+  </footer>
 </body>
 </html>
