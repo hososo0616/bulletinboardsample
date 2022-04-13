ec1e480396a9be3b542dcc1fedc9ed2b7c4c020d
hososo <hoso6616@gmail.com>
2022-04-13 13:35:08 +0900

    CSSの追加

--- index.php
+++ index.php
@@ -1,46 +1,54 @@
 <!DOCTYPE html>
-<html lang="ja">
+<html>
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>会員登録</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
+    <title>アカウント作成</title>
+    <link href="https://unpkg.com/sanitize.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-  <header>
-    <h1>会員登録</h1>
-  </header>
-  <main>
-  <p>次のフォームに必要事項をご記入ください</p>
-  <ol>
-    <li>
-      <p>ニックネーム</p>
-      <form>
-        <input type="text">
-      </form>
-    </li>
-    <li>
-      <p>メールアドレス</p>
-      <form>
-        <input type=" mail">
-      </form>
-    </li>
-    <li>
-      <p>パスワード</p>
-      <form>
-        <input type="text">
-      </form>
-    </li>
-    <li>
-      <p>写真など</p>
-      <form>
-        <input type="file">
-      </form>
-    </li>
-  </ol>
-  </main>
-  <footer>
+    <div class="content">
+        <form action="" method="POST">
+            <h1>アカウント作成</h1>
+            <p>当サービスをご利用するために、次のフォームに必要事項をご記入ください。</p>
+            <br>
+ 
+            <div class="control">
+                <label for="name">ユーザー名</label>
+                <input id="name" type="text" name="name">
+            </div>
+ 
+            <div class="control">
+                <label for="email">メールアドレス<span class="required">必須</span></label>
+                <input id="email" type="email" name="email">
+                <?php if (!empty($error["email"]) && $error['email'] === 'blank'): ?>
+                    <p class="error">＊メールアドレスを入力してください</p>
+                <?php elseif (!empty($error["email"]) && $error['email'] === 'duplicate'): ?>
+                    <p class="error">＊このメールアドレスはすでに登録済みです</p>
+                <?php endif ?>
+            </div>
+ 
+            <div class="control">
+                <label for="password">パスワード<span class="required">必須</span></label>
+                <input id="password" type="password" name="password">
+                <?php if (!empty($error["password"]) && $error['password'] === 'blank'): ?>
+                    <p class="error">＊パスワードを入力してください</p>
+                <?php endif ?>
+            </div>
 
-  </footer>
+            <div class="control">
+                <label for="photo">イメージ写真</label>
+                <input id="photo" type="file" name="photo">
+                <?php if (!empty($error["photo"]) && $error['photo'] === 'blank'): ?>
+                    <p class="error">＊パスワードを入力してください</p>
+                <?php endif ?>
+            </div>
+
+            <div class="control">
+                <button type="submit" class="btn">確認する</button>
+            </div>
+        </form>
+    </div>
 </body>
 </html>
