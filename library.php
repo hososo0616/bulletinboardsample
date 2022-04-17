235c8352003e4bdc9dea4917ca77c237e4cabffc
hososo <hoso6616@gmail.com>
2022-04-17 22:27:21 +0900

    4/17 heroku設定

--- library.php
+++ library.php
@@ -8,7 +8,8 @@
    //db接続
    function dbconnect() {
       // $db = new mysqli("localhost:8889", "root", "root", "boardsql");
-      $db = new PDO('mysql:dbname=boardsql;host=localhost:8889;charset=utf8', "root", "root");
+      // $db = new PDO('mysql:dbname=boardsql;host=localhost:8889;charset=utf8', "root", "root");
+      $db = new PDO('mysql:dbname=heroku_45b92d6d958d659;host=us-cdbr-east-05.cleardb.net;charset=utf8', "b221d50c2d2141", "759121d3");
 
       //データベースに接続できなかったら
       if (!$db) {
