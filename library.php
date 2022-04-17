b487b6cef5c4bf557ece4430193b230eeb387b23
hososo <hoso6616@gmail.com>
2022-04-18 02:26:54 +0900

    4/18編集

--- library.php
+++ library.php
@@ -9,7 +9,7 @@
    function dbconnect() {
       // $db = new mysqli("localhost:8889", "root", "root", "boardsql");
       // $db = new PDO('mysql:dbname=boardsql;host=localhost:8889;charset=utf8', "root", "root");
-      $db = new PDO('mysql:dbname=heroku_6de2d7937688b8b;host=us-cdbr-east-05.cleardb.net;charset=utf8', "b8d554ea577e0c", "6dd97f31");
+      $db = new PDO('mysql:dbname=heroku_7ae20a36ae108cf;host=us-cdbr-east-05.cleardb.net;charset=utf8', "be917dbecf859c", "8205b4c9");
 
       //データベースに接続できなかったら
       if (!$db) {
