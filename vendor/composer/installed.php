8f21f3b069aa339d428aeb1631e72fb5ebc6d220
hososo <hoso6616@gmail.com>
2022-04-18 03:01:55 +0900

    4/18composer.lock追加

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '8062bec5ccdfc4e54cb1c61b19dbd8804db4b7cf',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '8062bec5ccdfc4e54cb1c61b19dbd8804db4b7cf',
+            'dev_requirement' => false,
+        ),
+    ),
+);
